d1d446a0489577c4628e70c49117044ac5dd3d42
Sachin <sachindhimann71@gmail.com>
2024-04-15 15:09:39 +0000

    completed Header

--- youtube-clone/src/components/Header.jsx
+++ youtube-clone/src/components/Header.jsx
@@ -5,18 +5,23 @@ function Header() {
 
   return (
     <>
-      <div className=" flex items-center justify-between">
+      <div className=" flex items-center justify-between h-16 text-2xl">
         <div className='flex items-center'>
-      <i className="fa-solid fa-bars mx-2 text-xl"></i>
+      <i className="fa-solid fa-bars mx-2 "></i>
       <i className="fa-brands fa-youtube text-3xl text-red-500 mx-2"></i>
-      <p className='text-red-600 text-xl font-bold'>Youtube</p>
+      <p className='text-red-600 font-bold'>Youtube</p>
       </div>
       <div className="search flex gap-x-2 items-center justify-center">
-          <input type="text" name="" id="" placeholder='sky' className='h-8 w-96 border-black p-2 rounded-10'/>
+          <input type="text" name="" id="" placeholder='Search Youtube' className='text-xl h-12 w-96 border-2 border-gray-500 p-2xl rounded-3xl px-5'/>
           <i class="fa-solid fa-magnifying-glass"></i>
       </div>
-      <div className="otherIcon"></div>
+      <div className="otherIcon">
+      <i class="fa-solid fa-video mx-4"></i>
+      <i class="fa-regular fa-bell mx-4"></i>
+      <i class="fa-solid fa-user mx-4"></i>
       </div>
+      </div>
+      
     </>
   )
 }
